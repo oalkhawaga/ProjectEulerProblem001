16d6076ae2ca1e3e891135531b7aeee70d2c3466
nicomp42 <nicholdw@ucmail.uc.edu>
2024-10-24 11:10:52 -0400

    Add project files.

---
+++ ProjectEulerProblem001/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblem001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SolveProjectEulerProblem001());
+        }
+        /// <summary>
+        /// Find the sum of all the multiples of 3 or 5 below 1000
+        /// https://projecteuler.net/problem=1
+        /// </summary>
+        /// <returns>The solution to the problem</returns>
+        static int SolveProjectEulerProblem001()
+        {
+            int sum = 0;
+            for (int i =0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+    }
+}
